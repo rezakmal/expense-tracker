03c926cb44b1cf6a5aae7d05b8fbddeb021eb76c
Reza Akmal <rezajakmal@gmail.com>
2026-02-02 22:45:03 +0700

    feat: register auth and expense routes in application

--- src/app.js
+++ src/app.js
@@ -3,4 +3,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/expense", require("./routes/expense.routes"));
+
 module.exports = app;
